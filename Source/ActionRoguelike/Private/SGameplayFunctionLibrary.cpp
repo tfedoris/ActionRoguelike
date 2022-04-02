ad2020069bddd0c0cce5422f43666a5d844b0f8f
Tyler Fedoris <tylerfedoris@outlook.com>
2022-04-02 10:59:51 -0400

    lecture 15: commit new files that were accidentally omitted from last commit

---
+++ Source/ActionRoguelike/Private/SGameplayFunctionLibrary.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SGameplayFunctionLibrary.h"
+
+#include "SAttributeComponent.h"
+
+bool USGameplayFunctionLibrary::ApplyDamage(AActor* DamageCauser, AActor* TargetActor, float DamageAmount)
+{
+	USAttributeComponent* AttributeComp = USAttributeComponent::GetAttributes(TargetActor);
+	if (!AttributeComp)
+	{
+		return false;
+	}
+
+	return AttributeComp->ApplyHealthChange(DamageCauser, -DamageAmount);
+}
+
+bool USGameplayFunctionLibrary::ApplyDirectionalDamage(AActor* DamageCauser, AActor* TargetActor, float DamageAmount,
+	const FHitResult& HitResult)
+{
+	if (ApplyDamage(DamageCauser, TargetActor, DamageAmount))
+	{
+		UPrimitiveComponent* HitComp = HitResult.GetComponent();
+		if (HitComp && HitComp->IsSimulatingPhysics(HitResult.BoneName))
+		{
+			HitComp->AddImpulseAtLocation(-HitResult.ImpactNormal * 300000.f, HitResult.ImpactPoint, HitResult.BoneName);
+		}
+		return true;
+	}
+
+	return false;
+}
